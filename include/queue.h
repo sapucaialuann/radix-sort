253980ed1d7be24abbfebda21c1284499c18c830
Felipe Matheus <fmalbernaz@protonmail.com>
2019-05-02 23:36:36 -0300

    Adiciona funções de fila

--- include/queue.h
+++ include/queue.h
@@ -12,7 +12,8 @@ typedef struct queue{
 } queue;
 
 queue* queue_create();
-void enqueue(queue *q, int value);
+void enqueue(queue *q, unsigned int value);
 int dequeue(queue *q);
+void queue_dump(queue *q);
 
 #endif
--- src/queue.c
+++ src/queue.c
@@ -1,14 +1,62 @@
 #include "../include/queue.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 queue* queue_create(){
-    return NULL;
+    queue *q = malloc(sizeof(queue*));
+
+    if(q == NULL){
+        return NULL;
+    }
+
+    q->beginning = NULL;
+    q->end = NULL;
 }
 
-void enqueue(queue *q, int value){
+void enqueue(queue *q, unsigned int value){
+    node *n = malloc(sizeof(node*));
 
+    if(n == NULL){
+        printf("Value wasnt inserted");
+        return;
+    }
+
+    n->value = value;
+    n->next = NULL;
+
+    if(q->end != NULL){
+        q->end->next = n;
+    }
+
+    else{
+        q->beginning = n;
+    }
+
+    q->end = n;
 }
 
 int dequeue(queue *q){
-    return 0;
+    if(q->beginning == NULL){
+        return -1;
+    }
+
+    node *old = q->beginning;
+    q->beginning = q->beginning->next;
+
+    if(q->beginning == NULL){
+        q->end = NULL;
+    }
+
+    int temp = old->value;
+    free(old);
+    
+    return temp;
+}
+
+void queue_dump(queue *q){
+    for(node *i = q->beginning; i != NULL; i = i->next){
+        printf("%d -> ", i->value);
+    }
+
+    printf("NULL\n");
 }
